5100e38d13aff497759ffcc2bc60a19f5c226681
Mike Smith <89040888+smiggiddy@users.noreply.github.com>
2025-12-04 06:41:16 -0500

    feat: added queue logic and stuff

--- src/client/index.ts
+++ src/client/index.ts
@@ -1,22 +1,81 @@
 import amqp from "amqplib";
-import { clientWelcome } from "../internal/gamelogic/gamelogic.js";
+import {
+  clientWelcome,
+  getInput,
+  printClientHelp,
+  commandStatus,
+  printQuit,
+} from "../internal/gamelogic/gamelogic.js";
+import { GameState } from "../internal/gamelogic/gamestate.js";
+import { commandMove } from "../internal/gamelogic/move.js";
+import { commandSpawn } from "../internal/gamelogic/spawn.js";
 import { declareAndBind } from "../internal/pubsub/api.js";
 import { PauseKey } from "../internal/routing/routing.js";
 
 async function main() {
-  const rabbitConnString = process.env.NODE_ENV === 'local' ? "amqp://guest:guest@localhost:5672/" : "amqp://default_user_9ezjAPRkTII4zNCNVm1:i5a8KsnTFR9HPzk5JIVjo_SuhujOxN4j@smig-ca01.lab.smig.tech:5672/"
+  const rabbitConnString =
+    process.env.NODE_ENV === "local"
+      ? "amqp://guest:guest@localhost:5672/"
+      : "amqp://default_user_9ezjAPRkTII4zNCNVm1:i5a8KsnTFR9HPzk5JIVjo_SuhujOxN4j@smig-ca01.lab.smig.tech:5672/";
   console.log("Starting Peril client...");
 
   const conn = await amqp.connect(rabbitConnString);
   const username = await clientWelcome();
 
   try {
-    declareAndBind(conn, 'peril_direct', `pause.${username}`, PauseKey, 'transient');
+    declareAndBind(
+      conn,
+      "peril_direct",
+      `pause.${username}`,
+      PauseKey,
+      "transient",
+    );
   } catch (err) {
     console.error(`Error creating queue`);
   }
+  const gs = new GameState(username);
 
+  let stopGame = false;
 
+  while (!stopGame) {
+    printClientHelp();
+    const input = await getInput();
+    switch (input[0]) {
+      case "spawn":
+        try {
+          commandSpawn(gs, input);
+        } catch (err) {
+          console.log(err);
+        }
+        continue;
+      case "move":
+        try {
+          commandMove(gs, input);
+        } catch (err) {
+          console.log(err);
+        }
+        continue;
+      case "status":
+        commandStatus(gs);
+        continue;
+      case "help":
+        printClientHelp();
+        continue;
+      case "quit":
+        printQuit();
+        stopGame = true;
+        break;
+      case "spam":
+        continue;
+      default:
+        console.log("enter a valid command");
+        printClientHelp();
+        continue;
+    }
+  }
+  if (stopGame) {
+    process.exit(0);
+  }
 }
 
 main().catch((err) => {
--- src/server/index.ts
+++ src/server/index.ts
@@ -1,38 +1,98 @@
-import amqp from "amqplib"
+import amqp from "amqplib";
+import { getInput, printServerHelp } from "../internal/gamelogic/gamelogic.js";
 import type { PlayingState } from "../internal/gamelogic/gamestate.js";
 import { publishJSON } from "../internal/pubsub/api.js";
-import { ExchangePerilDirect, PauseKey } from "../internal/routing/routing.js";
+import {
+  ExchangePerilDirect,
+  ExchangePerilTopic,
+  PauseKey,
+} from "../internal/routing/routing.js";
+import { declareAndBind } from "../internal/pubsub/api.js";
 
-
-
-
-const rabbitConnString = process.env.NODE_ENV === 'local' ? "amqp://guest:guest@localhost:5672/" : "amqp://default_user_9ezjAPRkTII4zNCNVm1:i5a8KsnTFR9HPzk5JIVjo_SuhujOxN4j@smig-ca01.lab.smig.tech:5672/"
+const rabbitConnString =
+  process.env.NODE_ENV === "local"
+    ? "amqp://guest:guest@localhost:5672/"
+    : "amqp://default_user_9ezjAPRkTII4zNCNVm1:i5a8KsnTFR9HPzk5JIVjo_SuhujOxN4j@smig-ca01.lab.smig.tech:5672/";
 
 async function main() {
   console.log("Starting Peril server...");
-  const conn = await amqp.connect(rabbitConnString)
-  console.log("connected to rabbit mq")
-  const channel = await conn.createConfirmChannel()
-  const playingState: PlayingState = {
-    isPaused: true
-  }
-  process.on('SIGINT', () => {
-    conn.close()
-    console.log("RabbitMQ Connection Closed")
-    process.exit(0)
-  })
 
+  const conn = await amqp.connect(rabbitConnString);
+  console.log("connected to rabbit mq");
+
+  process.on("SIGINT", () => {
+    conn.close();
+    console.log("RabbitMQ Connection Closed");
+    process.exit(0);
+  });
+  process.on("SIGINT", () => {
+    conn.close();
+    console.log("RabbitMQ Connection Closed");
+    process.exit(0);
+  });
+
+  const channel = await conn.createConfirmChannel();
+  await channel.assertExchange(ExchangePerilDirect, "direct");
+  await channel.assertExchange(ExchangePerilTopic, "topic");
 
   try {
-    console.log(ExchangePerilDirect, PauseKey, playingState)
-    await publishJSON(channel, ExchangePerilDirect, PauseKey, playingState)
-
+    declareAndBind(
+      conn,
+      ExchangePerilTopic,
+      "game_logs",
+      "game_logs.*",
+      "durable",
+    );
   } catch (err) {
-    console.error("Error publishing message", err)
+    console.error(`Error creating queue`);
   }
 
+  printServerHelp();
 
+  let stopGame = false;
+  while (!stopGame) {
+    const input = await getInput();
 
+    for (const item of input) {
+      switch (item) {
+        case "pause":
+          console.log("Sending a pause message", item);
+          await publishJSON(channel, ExchangePerilDirect, PauseKey, {
+            isPaused: true,
+          });
+          continue;
+
+        case "resume":
+          console.log("Sending a resume message", item);
+          await publishJSON(channel, ExchangePerilDirect, PauseKey, {
+            isPaused: false,
+          });
+          continue;
+        case "quit":
+          console.log("Exiting the game", item);
+          stopGame = true;
+          break;
+        case "":
+          console.log("Please enter a valid command. Options are:");
+          printServerHelp();
+          continue;
+        default:
+          continue;
+      }
+    }
+  }
+
+  if (stopGame) {
+    process.exit(0);
+  }
+
+  // try {
+  //   console.log(ExchangePerilDirect, PauseKey, playingState)
+  //   await publishJSON(channel, ExchangePerilDirect, PauseKey, playingState)
+  //
+  // } catch (err) {
+  //   console.error("Error publishing message", err)
+  // }
 }
 
 main().catch((err) => {
